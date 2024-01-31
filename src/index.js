d9e2df55a1bc2a12616891a2d261df9d3e3a3dbe
Pengu1Nus <mustafin.zulfat@yandex.ru>
2024-01-31 23:51:39 +0300

    refactor: separate read file and folders navigation into separate files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import readline from 'readline';
 import os from 'os';
-import { getPath } from "./utils/pathSolver.js";
+import { changeDirectory, upToDirectory } from './nwd/navigation.js';
+import { cat } from './fs/readFile.js';
 
-
-const { __filename, __dirname } = getPath(import.meta.url);
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  prompt: '>>> '
+  prompt: '>>> ',
 });
 
 try {
@@ -17,39 +16,31 @@ try {
   console.error(`Error changing directory:`, err);
 }
 
-rl.prompt()
+rl.prompt();
 
 rl.on('line', (input) => {
-  const [cmd, args] = input.trim().split(' ');
+  const [cmd, ...args] = input.trim().split(' ');
   switch (cmd) {
     case 'up':
-      if (process.cwd() !== os.homedir()) {
-        process.chdir('..');
-        console.log(`Change directory to ${process.cwd()}`);
-      }
+      upToDirectory();
       break;
     case 'cd':
-      const inputPath = path.join(process.cwd(), args);
-      console.log(inputPath);
-      try {
-        process.chdir(inputPath);
-        console.log(`Change directory to ${process.cwd()}`);
-      } catch (err) {
-
-      }
+      changeDirectory(args);
+      break;
     case 'cp':
-      console.log('Copy file');
+      console.log(`You are currently in ${process.cwd()}`);
       break;
     case 'cat':
-      console.log('Read file');
+      console.log(`You are currently in ${process.cwd()}`);
+      cat(args[0]);
       break;
     case '.exit':
       rl.close();
     default:
       console.log('Incorrect input');
   }
-  rl.prompt()
+  rl.prompt();
 }).on('close', () => {
   console.log('Thank you for using File Manager, Username, goodbye!');
   process.exit(0);
-})
+});
